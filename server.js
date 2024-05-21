35848643f0ea455dbdb22f1503daf390591dc5a6
Jace <sockgnome@att.net>
2024-05-21 15:20:16 +0000

    Add weather route stub to server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/hello/:name', (req, res) => {
   res.send(`Hello ${req.params.name}!`);
 });
 
+// Weather route
+app.get('/weather', (req, res) => {
+  res.send('The weather is nice today!');
+});
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
