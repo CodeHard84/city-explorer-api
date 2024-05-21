1e05c708c61ac6755e0027e5b3900b6ef5f8feb6
Jace <sockgnome@att.net>
2024-05-21 17:03:54 +0000

    feat: Refactor weather route to use Weather class and handle error cases

--- server.js
+++ server.js
@@ -8,36 +8,46 @@ const weatherData = require('./data/weather.json');
 
 // Home route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).send('Hello World!');
 });
 
 // Hello name route
 app.get('/hello/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}!`);
+  res.status(200).send(`Hello ${req.params.name}!`);
 });
 
 // Weather route
 app.get('/weather', (req, res) => {
-  const city = req.query.city;
-  if (city) {
-    const weather = new Weather(city);
-    res.json(weather.weatherInfo);
+  const { searchQuery, lat, lon } = req.query;
+  if (searchQuery && lat && lon) {
+    const weather = new Weather(searchQuery, lat, lon);
+    res.status(200).json(weather.weatherInfo);
   } else {
-    res.status(400).send('Please specify a city in the query string. Example: /weather?city=Seattle');
+    res.status(400).send('Please specify searchQuery, lat, and lon in the query string. Example: /weather?searchQuery=Seattle&lat=47.6038321&lon=-122.330062');
   }
 });
 
 class Weather {
-  constructor(city) {
-    this.city = city;
-    this.weatherInfo = this.getWeather(city);
+  constructor(searchQuery, lat, lon) {
+    this.searchQuery = searchQuery;
+    this.lat = lat;
+    this.lon = lon;
+    this.weatherInfo = this.getWeather();
   }
 
-  getWeather(city) {
-    if (!city) {
-      return { error: 'City not provided' };
+  getWeather() {
+    const { searchQuery, lat, lon } = this;
+    if (!searchQuery || !lat || !lon) {
+      return { error: 'searchQuery, lat, or lon not provided' };
     }
-    const cityWeather = weatherData.find(item => item.city_name && item.city_name.toLowerCase() === city.toLowerCase());
+
+    const cityWeather = weatherData.find(item => 
+      item.city_name && 
+      item.city_name.toLowerCase() === searchQuery.toLowerCase() &&
+      item.lat.toString() === lat.toString() &&
+      item.lon.toString() === lon.toString()
+    );
+
     if (cityWeather) {
       return cityWeather;
     } else {
@@ -45,6 +55,16 @@ class Weather {
     }
   }
 }
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).send(error.message);
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
