4b9efe5dd3adadaa3b793fee8a395437840da03a
Jace <sockgnome@att.net>
2024-05-21 15:19:17 +0000

    feat: Add new route for saying hello to a specific name

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 4040;
 
+// Home route
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Hello name route
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}!`);
 });
 
 app.listen(port, () => {
