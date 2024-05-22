1aa9aec1e5a89e48a635e908f8dfc402722edb23
Jace <sockgnome@att.net>
2024-05-22 00:05:04 +0000

    chore: Deploy to render.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Load data/weather.json into an array
+// Load data/weather.json into an array.
 const weatherData = require('./data/weather.json');
 
 // Middleware
