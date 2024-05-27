2521d22eeaedce3d1214c89ca69b889c1d5ffb95
Jace <sockgnome@att.net>
2024-05-27 04:02:37 +0000

    refactor: Move weather functionality to separate module

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv').config();
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+// Import the weather module
+const { Forecast } = require('./weather');
 
 // Middleware
 const cors = require('cors');
@@ -12,11 +14,6 @@ app.use(cors({ origin: '*' }));
 // Home route
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
-});
-
-// Hello name route
-app.get('/hello/:name', (req, res) => {
-  res.status(200).send(`Hello ${req.params.name}!`);
 });
 
 // Weather route
@@ -77,39 +74,6 @@ class Movies {
       throw new Error('Error getting movie data.');
     }
   }
-}
-
-class Forecast {
-  constructor(searchQuery, lat, lon) {
-    this.lat = lat;
-    this.lon = lon;
-  }
-
-  async getForecast() {
-    try {
-      const response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily`, {
-        params: {
-          lat: this.lat,
-          lon: this.lon,
-          key: WEATHER_API_KEY
-        }
-      });
-      const weatherData = response.data.data;
-
-      return weatherData.map(day => ({
-        description: `Low of ${celsiusToFahrenheit(day.low_temp)}°F, high of ${celsiusToFahrenheit(day.high_temp)}°F with ${day.weather.description}.`,
-        date: day.valid_date
-      }));
-    } catch (error) {
-      console.log(error);
-      throw new Error('Error getting weather data.');
-    }
-  }
-}
-
-// Convert Celsius to Fahrenheit
-function celsiusToFahrenheit(celsius) {
-  return Math.round((celsius * 9) / 5 + 32);
 }
 
 // Error handling middleware
